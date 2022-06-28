a8cad2fdead630c27393dc11b67316ebef70a348
TRG-T <lockontargt@gmail.com>
2022-06-28 23:30:29 +0200

    printing blank board

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    print_board()
+}
+
+fn print_board() {
+    //rows
+    for i in 0..=32 {
+        //cols
+        for j in 0..=32 {
+            pick_char(j, i);
+        }
+        print!("\n");
+    }
+}
+
+fn pick_char(a: u8, b: u8) {
+    if a % 8 == 0 && b % 4 == 0 {
+        print!("+");
+    } else if a % 4 == 0 {
+        print!("|");
+    } else if b % 4 == 0 {
+        print!("-")
+    } else {
+        print!(" ");
+    }
 }
