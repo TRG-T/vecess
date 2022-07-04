cfc19e35c9f66e02737ae00949d40905ffed1fbd
TRG-T <lockontargt@gmail.com>
2022-07-04 10:10:18 +0200

    infinite loop

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use colored::Colorize;
+use std::{thread, time::{Duration}};
 
 
 const BOARD_SIZE: usize = 8;
@@ -7,12 +8,27 @@ const LETTERS: [char; 8] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 // 0,102,204 - brighter field
 const COLORS: [[u8;3];2] = [[0,154, 255], [0,102,204]];
 
+fn fps(fps: u64) -> Duration {
+    Duration::from_millis(1000/fps)
+}
+
 fn main() {
     let mut board: [[&str; 8]; 8] = [["   "; 8]; 8];
+    let mut white_move = true;
     generate_pieces(&mut board, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
     println!();
-    print_board_letters();
-    print_board(board);
+
+    loop {
+        print!("{esc}[2J{esc}[5;1H", esc = 27 as char);
+        print_board_letters();
+        print_board(board);
+        println!();
+        match white_move {
+            true => println!("       White's move"),
+            false => println!("      Black's move")
+        }
+        thread::sleep(fps(5));
+    }
 }
 
 fn print_board(board: [[&str; 8]; 8]) {
@@ -27,8 +43,8 @@ fn print_board(board: [[&str; 8]; 8]) {
 
 fn print_board_letters() {
     print!("  ");
-    for a in 0..BOARD_SIZE {
-        print!(" {} ", LETTERS[a]);
+    for letter in LETTERS {
+        print!(" {} ", letter);
     }
     println!();
 }
@@ -49,19 +65,19 @@ fn generate_pieces(board: &mut [[&str; 8]; 8], pieces: &str) {
         }
         match c {
             // black pieces
-            'r' => board[row][col] = " ♜ ",
-            'n' => board[row][col] = " ♞ ",
-            'b' => board[row][col] = " ♝ ",
-            'q' => board[row][col] = " ♛ ",
-            'k' => board[row][col] = " ♚ ",
-            'p' => board[row][col] = " ♟ ",
+            'r' => board[row][col] = " ♖ ",
+            'n' => board[row][col] = " ♘ ",
+            'b' => board[row][col] = " ♗ ",
+            'q' => board[row][col] = " ♕ ",
+            'k' => board[row][col] = " ♔ ",
+            'p' => board[row][col] = " ♙ ",
             // white pieces
-            'R' => board[row][col] = " ♖ ",
-            'N' => board[row][col] = " ♘ ",
-            'B' => board[row][col] = " ♗ ",
-            'Q' => board[row][col] = " ♕ ",
-            'K' => board[row][col] = " ♔ ",
-            'P' => board[row][col] = " ♙ ",
+            'R' => board[row][col] = " ♜ ",
+            'N' => board[row][col] = " ♞ ",
+            'B' => board[row][col] = " ♝ ",
+            'Q' => board[row][col] = " ♛ ",
+            'K' => board[row][col] = " ♚ ",
+            'P' => board[row][col] = " ♟ ",
             _ => {}
         }
         col += 1;
