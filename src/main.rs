f03ed7a202738f66b8bafad26493dcabfdb4d5be
TRG-T <lockontargt@gmail.com>
2022-07-06 17:19:22 +0200

    better colored pieces

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use crossterm::{
     cursor::{Hide, MoveTo},
     event::{poll, read, Event, KeyCode},
     execute,
-    style::{Color, Stylize},
+    style::{Color, Stylize, StyledContent},
     terminal::{enable_raw_mode, Clear, ClearType, EnterAlternateScreen},
 };
 use std::{
@@ -13,8 +13,6 @@ use std::{
 
 const BOARD_SIZE: usize = 8;
 const LETTERS: [char; 8] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-// 0,154,255 - darker field
-// 0,102,204 - brighter field
 const COLORS: [Color; 3] = [Color::DarkBlue, Color::Blue, Color::Green];
 
 fn fps(fps: u64) -> Duration {
@@ -22,7 +20,7 @@ fn fps(fps: u64) -> Duration {
 }
 
 fn main() -> Result<(),Error>{
-    let mut board: [[&str; 8]; 8] = [["   "; 8]; 8];
+    let mut board: [[StyledContent<&str>; 8]; 8] = [["   ".black(); 8]; 8];
     let white_move = true;
     let mut cursor: (usize, usize) = (7, 4);
 
@@ -46,13 +44,17 @@ fn main() -> Result<(),Error>{
                 Event::Key(event) => match event.code {
                     KeyCode::Char('q') => break,
                     KeyCode::Char('w') => cursor.0 -= 1,
+                    KeyCode::Char('s') => cursor.0 += 1,
+                    KeyCode::Char('a') => cursor.1 -= 1,
+                    KeyCode::Char('d') => cursor.1 += 1,
+                    //KeyCode::Enter => show_moves(&mut board, cursor),
                     _ => {}
                 },
                 Event::Mouse(event) => println!("{:?}", event),
                 Event::Resize(width, height) => println!("New size {}x{}", width, height),
             }
         }
-        thread::sleep(fps(5));
+        thread::sleep(fps(10));
     }
     Ok(())
 }
@@ -68,7 +70,7 @@ fn clear_terminal() -> Result<(), Error> {
     Ok(())
 }
 
-fn print_board(board: [[&str; 8]; 8], cursor: (usize, usize)) {
+fn print_board(board: [[StyledContent<&str>; 8]; 8], cursor: (usize, usize)) {
     for row in 0..BOARD_SIZE {
         print!("{} ", 8 - row);
         for col in 0..BOARD_SIZE {
@@ -86,7 +88,7 @@ fn print_board_letters() {
     println!("\r");
 }
 
-fn pick_char(row: usize, col: usize, board: [[&str; 8]; 8], cursor: (usize, usize)) {
+fn pick_char(row: usize, col: usize, board: [[StyledContent<&str>; 8]; 8], cursor: (usize, usize)) {
     let color = COLORS[(row + col) % 2];
     if cursor == (row, col) {
         print!("{}", board[row][col].on(COLORS[2]));
@@ -95,7 +97,7 @@ fn pick_char(row: usize, col: usize, board: [[&str; 8]; 8], cursor: (usize, usiz
     print!("{}", board[row][col].on(color));
 }
 
-fn generate_pieces(board: &mut [[&str; 8]; 8], pieces: &str) {
+fn generate_pieces(board: &mut [[StyledContent<&str>; 8]; 8], pieces: &str) {
     let mut row: usize = 0;
     let mut col: usize = 0;
     for c in pieces.chars() {
@@ -106,19 +108,19 @@ fn generate_pieces(board: &mut [[&str; 8]; 8], pieces: &str) {
         }
         match c {
             // black pieces
-            'r' => board[row][col] = " ♖ ",
-            'n' => board[row][col] = " ♘ ",
-            'b' => board[row][col] = " ♗ ",
-            'q' => board[row][col] = " ♕ ",
-            'k' => board[row][col] = " ♔ ",
-            'p' => board[row][col] = " ♙ ",
+            'r' => board[row][col] = " ♜ ".black(),
+            'n' => board[row][col] = " ♞ ".black(),
+            'b' => board[row][col] = " ♝ ".black(),
+            'q' => board[row][col] = " ♛ ".black(),
+            'k' => board[row][col] = " ♚ ".black(),
+            'p' => board[row][col] = " ♟ ".black(),
             // white pieces
-            'R' => board[row][col] = " ♜ ",
-            'N' => board[row][col] = " ♞ ",
-            'B' => board[row][col] = " ♝ ",
-            'Q' => board[row][col] = " ♛ ",
-            'K' => board[row][col] = " ♚ ",
-            'P' => board[row][col] = " ♟ ",
+            'R' => board[row][col] = " ♜ ".white(),
+            'N' => board[row][col] = " ♞ ".white(),
+            'B' => board[row][col] = " ♝ ".white(),
+            'Q' => board[row][col] = " ♛ ".white(),
+            'K' => board[row][col] = " ♚ ".white(),
+            'P' => board[row][col] = " ♟ ".white(),
             _ => {}
         }
         col += 1;
