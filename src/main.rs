6a43c1b5556613942dc0bd5b98c52153facd891b
TRG-T <lockontargt@gmail.com>
2022-07-08 11:49:25 +0200

    Piece class

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,26 @@ use std::{
 const BOARD_SIZE: usize = 8;
 const LETTERS: [char; 8] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 const COLORS: [Color; 4] = [Color::DarkBlue, Color::Blue, Color::Green, Color::DarkGrey];
+type Board<'a> = [[Piece<'a>;8];8];
 
 fn fps(fps: u64) -> Duration {
     Duration::from_millis(1000 / fps)
 }
 
+#[derive(Clone, Copy)]
+struct Piece<'a>{
+    char: StyledContent<&'a str>,
+    has_moved: bool
+}
+
+impl<'a> Piece<'a> {
+    pub fn new(char: &'a str, fg_color: Color, bg_color: Color) -> Piece<'a> {
+        Piece { char: char.with(fg_color).on(bg_color), has_moved: false }
+    }
+}
+
 fn main() -> Result<(),Error>{
-    let mut board: [[StyledContent<&str>; 8]; 8] = [["   ".black(); 8]; 8];
+    let mut board: Board = [[Piece::new("  ", Color::Black, Color::Black); 8]; 8];
     let white_move = true;
     let mut cursor: (usize, usize) = (7, 4);
 
@@ -47,7 +60,9 @@ fn main() -> Result<(),Error>{
                     KeyCode::Char('s') => cursor.0 = (cursor.0+1)%8,
                     KeyCode::Char('a') => cursor.1 = (cursor.1+7)%8,
                     KeyCode::Char('d') => cursor.1 = (cursor.1+1)%8,
-                    KeyCode::Enter => show_moves(&mut board, cursor),
+                    KeyCode::Enter => {
+
+                    },
                     _ => {}
                 },
                 Event::Mouse(event) => println!("{:?}", event),
@@ -70,7 +85,7 @@ fn clear_terminal() -> Result<(), Error> {
     Ok(())
 }
 
-fn print_board(board: [[StyledContent<&str>; 8]; 8], cursor: (usize, usize)) {
+fn print_board(board: Board, cursor: (usize, usize)) {
     for row in 0..BOARD_SIZE {
         print!("{} ", 8 - row);
         for col in 0..BOARD_SIZE {
@@ -88,15 +103,15 @@ fn print_board_letters() {
     println!("\r");
 }
 
-fn print_board_pieces(row: usize, col: usize, board: [[StyledContent<&str>; 8]; 8], cursor: (usize, usize)) {
+fn print_board_pieces(row: usize, col: usize, board: Board, cursor: (usize, usize)) {
     if cursor == (row, col) {
-        print!("{}", board[row][col].on(COLORS[2]));
+        print!("{}", board[row][col].char.on(COLORS[2]));
         return;
     }
-    print!("{}", board[row][col]);
+    print!("{}", board[row][col].char);
 }
 
-fn generate_board(board: &mut [[StyledContent<&str>; 8]; 8], pieces: &str) {
+fn generate_board(board: &mut Board, pieces: &str) {
     let mut row: usize = 0;
     let mut col: usize = 0;
     for c in pieces.chars() {
@@ -108,44 +123,28 @@ fn generate_board(board: &mut [[StyledContent<&str>; 8]; 8], pieces: &str) {
         let mut color = COLORS[(row + col) % 2];
         match c {
             // black pieces
-            'r' => board[row][col] = " ♜ ".black().on(color),
-            'n' => board[row][col] = " ♞ ".black().on(color),
-            'b' => board[row][col] = " ♝ ".black().on(color),
-            'q' => board[row][col] = " ♛ ".black().on(color),
-            'k' => board[row][col] = " ♚ ".black().on(color),
-            'p' => board[row][col] = " ♟ ".black().on(color),
+            'r' => board[row][col] = Piece::new(" ♜ ", Color::Black, color),
+            'n' => board[row][col] = Piece::new(" ♞ ", Color::Black, color),
+            'b' => board[row][col] = Piece::new(" ♝ ", Color::Black, color),
+            'q' => board[row][col] = Piece::new(" ♛ ", Color::Black, color),
+            'k' => board[row][col] = Piece::new(" ♚ ", Color::Black, color),
+            'p' => board[row][col] = Piece::new(" ♟ ", Color::Black, color),
             // white pieces
-            'R' => board[row][col] = " ♜ ".white().on(color),
-            'N' => board[row][col] = " ♞ ".white().on(color),
-            'B' => board[row][col] = " ♝ ".white().on(color),
-            'Q' => board[row][col] = " ♛ ".white().on(color),
-            'K' => board[row][col] = " ♚ ".white().on(color),
-            'P' => board[row][col] = " ♟ ".white().on(color),
+            'R' => board[row][col] = Piece::new(" ♜ ", Color::White, color),
+            'N' => board[row][col] = Piece::new(" ♞ ", Color::White, color),
+            'B' => board[row][col] = Piece::new(" ♝ ", Color::White, color),
+            'Q' => board[row][col] = Piece::new(" ♛ ", Color::White, color),
+            'K' => board[row][col] = Piece::new(" ♚ ", Color::White, color),
+            'P' => board[row][col] = Piece::new(" ♟ ", Color::White, color),
             '8' => {
                 for a in 0..8 {
                     color = COLORS[(row+a)%2];
-                    board[row][a] = "   ".on(color)
+                    board[row][a] = Piece::new("   ", Color::White, color);
                 }
             },
             _ => {}
         }
         col += 1;
-    }
-}
-
-fn show_moves(board: &mut [[StyledContent<&str>; 8]; 8], cursor: (usize, usize)) {
-    //"   ".on_grey()
-    println!("{}", board[cursor.0][cursor.1].content());
-    match *board[cursor.0][cursor.1].content() {
-        " ♟ " => {
-            for a in 1..=2 {
-                change_background_color(&mut board[cursor.0-a][cursor.1], COLORS[3])
-            }
-        },
-        " ♜ " => {
-            board[cursor.0-1][cursor.1] = board[cursor.0-1][cursor.1].on(COLORS[3]);
-        },
-        _ => {}
     }
 }
 
